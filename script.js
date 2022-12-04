d02909cd5a45c721f3f0e6e82ec587c50cf29b54
Mubashir Ali <mubashir@DebianTower.SUPAHOT500.net>
2022-12-04 18:42:38 +0000

    Final commit for calculator button and key input functionality.

--- script.js
+++ script.js
@@ -11,8 +11,14 @@ const operatorBtn = document.getElementsByClassName('operator')
 
 const numberBtnArray = Array.from(numberBtn)
 const operatorBtnArray = Array.from(operatorBtn)
-const numRegEx = /\d+/
-const operatorRegEx = /\÷\×\-\+/;
+
+const screenLimit = 30
+let baseScreen = true
+let screenCount = 0
+let lastInput = workingScreen.textContent[screenCount]
+let activeDecimal = false
+let activeOperation = false
+let activeOperators = 0
 
 window.addEventListener('keydown', keyInput)
 equalsBtn.addEventListener('click', evaluate)
@@ -29,31 +35,62 @@ operatorBtnArray.forEach((button) =>
     button.addEventListener('click', () => appendOperator(button.textContent)))
 
 function appendNumber(number) {
-    if (workingScreen.textContent === '0') {
+    if (screenCount === screenLimit) {
+    }
+    else if (baseScreen === true) {
         workingScreen.textContent = number
-    } else {
+        baseScreen = false
+        screenCount += 1
+        activeOperation = false
+    }
+    else {
         workingScreen.textContent += number
+        baseScreen = false
+        screenCount += 1
+        activeOperation = false
     }
 }
 
 function appendOperator(operator) {
-    if (workingScreen.textContent.includes('÷'||'×'||'-'||'+')) {
-    } else {
+    if (activeOperation || screenCount === screenLimit) {
+    }
+    else if (activeOperators > 3) {
+    alert('Maximum of 4 active operators.')
+    }
+    else {
     workingScreen.textContent += operator
+    baseScreen = false
+    screenCount += 1
+    activeOperation = true
+    activeOperators += 1
+    activeDecimal = false
     }
 }
 
 function allClear() {
-    workingScreen.textContent = '0'
     history.textContent = ''
+    clearInput()
 }
 
 function clearInput() {
     workingScreen.textContent = '0'
+    baseScreen = true
+    screenCount = 0
+    activeDecimal = false
+    activeOperation = false
+    activeOperators = 0
 }
 
 function deleteInput() {
-    workingScreen.textContent[-1] = ''
+    if (lastInput === '+' || lastInput === '-' || lastInput === '×' || lastInput === '÷') {
+        activeOperation = false
+        activeOperators -= 1
+    }
+    workingScreen.textContent = workingScreen.textContent.slice(0, -1)
+    if (lastInput === '.') {
+        screenCount -= 1
+        activeDecimal = true
+    }
 }
 
 function prevAnswer() {
@@ -61,27 +98,64 @@ function prevAnswer() {
 }
 
 function appendDecimal() {
-    if (workingScreen.textContent[-1].match(/d/) && !(workingScreen.textContent.includes('.'))) {
-        workingScreen.textContent += '.'
+    if (activeDecimal === true || screenCount === screenLimit) {
     } else {
+    workingScreen.textContent += '.'
+    baseScreen = false
+    screenCount += 1
+    activeDecimal = true
     }
-    // if (workingScreen.textContent.includes('.')) {
-    //     workingScreen.textContent
-    // } else {
-    //     workingScreen.textContent += '.'
-    // }
+}
+
+function keyInput(e) {
+    if (e.key >= 0 && e.key <= 9) appendNumber(e.key)
+    if (e.key === '.') appendDecimal()
+    if (e.key === '=' || e.key === 'Enter') evaluate()
+    if (e.key === 'Backspace') deleteInput()
+    if (e.key === 'Escape') allClear()
+    if (e.key === '+' || e.key === '-') appendOperator(e.key)
+    if (e.key === '*') appendOperator('×')
+    if (e.key === '/') appendOperator('÷')
+}
+
+function addition(arg1, arg2) {
+    return(arg1 + arg2)
+}
+
+function subtraction(arg1, arg2) {
+    return(arg1 - arg2)
+}
+
+function multiplication(arg1, arg2) {
+    return(arg1 * arg2)
+}
+
+function division(arg1, arg2) {
+    return(arg1 / arg2)
 }
 
 function evaluate() {
-    workingScreen.textContent
-    resultReturn(answer)
+    let values =  workingScreen.textContent.split(/\D+/)
+    let operators = workingScreen.textContent.split(/\d+/)
+    let answer = parseInt(values[0])
+    for (i=1; i<operators.length-1; i++) {
+        if (operators[i] === '+') {
+            answer = addition(answer, parseInt(values[i]))
+        }
+        else if (operators[i] === '-') {
+            answer = subtraction(answer, parseInt(values[i]))
+        }
+        else if (operators[i] === '×') {
+            answer = multiplication(answer, parseInt(values[i]))
+        }
+        else if (operators[i] === '÷') {
+            answer = division(answer, parseInt(values[i]))
+        }
+    }
+    returnResult(answer)
 }
 
-function resultReturn(result) {
+function returnResult(result) {
     history.textContent = result
-    workingScreen.textContent = '0'
-}
-
-function keyInput() {
-
+    clearInput()
 }
